a64492de5163fea5006fa9adc4b63461ef7a7ef5
VANKOCKO <van.kocko@gmail.com>
2018-06-04 13:00:41 +0200

    test

---
+++ Introduction/variables.php
@@ -0,0 +1,57 @@
+<?php
+
+
+
+ /**
+  *   Declaration des variables 
+  */
+
+
+
+    /**
+     *    Varibale de type String 
+     */
+
+     $nom_du_visiteur = "mateo21";
+     $variable="Mon \"nom\" est mateo21";
+ 
+     
+    /**
+      *    Varibale de type int  
+      */
+      
+     $_age_du_visituer =17;
+
+    /**
+      *    Varibale de type float  
+      */
+
+       
+     $poids = 57.3;
+
+    /**
+     *    Varibale de type bool 
+     */
+     
+     $je_suis_un_zero = false;
+     $je_suis_un_bon_en_php= true;
+
+
+
+/**
+ *   Addition, Soustraction, Multiplication, Division
+ */
+
+ echo " Addition 2+ 3 = ".$Addition = 2+3 ."<br>"; 
+ 
+ echo " Soustraction 4 - 3 = ".$Soustraction = 4-3 ."<br>";
+
+ echo " Multiplication 7 * 3 = ".$Multiplication = 7*3 ."<br>";
+
+ echo " Division 7 / 3 = ".$Division = 7/3 ."<br>";
+
+
+
+    
+
+?>
